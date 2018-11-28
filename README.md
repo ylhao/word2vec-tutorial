ba06052307428ee064223a88306875c246aca944
yilonghao <765422195@qq.com>
2018-11-28 04:15:33 -0500

    2018/11/28

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # MaleWare Detection
 
+## 脚本
+
+- add_prefix.py: 给文件添加你前缀
+- exe_to_bytes.py: exe 文件转字节码序列
+
 ## 文章
 
 - [准确率99%！基于深度学习的二进制恶意样本检测](http://www.hansight.com/blog-deepsense-virus-detection.html)
@@ -8,12 +13,7 @@
 - [分析恶意软件的加壳技巧](http://www.mottoin.com/103075.html)
 - [专家教你利用深度学习检测恶意代码](https://zhuanlan.zhihu.com/p/32251097)
 - [利用机器学习进行恶意代码分类](https://bindog.github.io/blog/2015/08/20/microsoft-malware-classification/)
-
-
-## GitHub 资源
-
 - [超赞的恶意软件分析](https://github.com/sunnyelf/awesome-malware-analysis/blob/master/%E8%B6%85%E8%B5%9E%E7%9A%84%E6%81%B6%E6%84%8F%E8%BD%AF%E4%BB%B6%E5%88%86%E6%9E%90.md)
 - [恶意软件分析大合集](https://github.com/rshipp/awesome-malware-analysis/blob/master/%E6%81%B6%E6%84%8F%E8%BD%AF%E4%BB%B6%E5%88%86%E6%9E%90%E5%A4%A7%E5%90%88%E9%9B%86.md)
 - [EMBER](https://github.com/endgameinc/ember)
-- [pefile](https://github.com/erocarrera/pefile)：一个分析 pe 文件的工具
 
---
+++ utils/exe_to_bytes.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+
+
+import os
+import time
+from multiprocessing import Pool
+
+
+NORMAL_EXE_PATH = 'D:/malware_detection_dataset/normal'
+MALWARE_EXE_PATH = 'D:/malware_detection_dataset/malware'
+NORMAL_BYTE_PATH = 'D:/malware_detection_dataset/normal_byte'
+MALWARE_BYTE_PATH = 'D:/malware_detection_dataset/malware_byte'
+# TEST_EXE_PATH = 'D:/malware_detection_dataset/test'
+# TEST_BYTE_PATH = 'D:/malware_detection_dataset/test_byte'
+MAX_PROCESS_NUM = 2  # 最大进程数
+
+
+# 如果文件夹不存在则创建对应的文件夹
+if not os.path.exists(NORMAL_BYTE_PATH):
+    os.makedirs(NORMAL_BYTE_PATH)
+if not os.path.exists(MALWARE_BYTE_PATH):
+    os.makedirs(MALWARE_BYTE_PATH)
+# if not os.path.exists(TEST_BYTE_PATH):
+    # os.makedirs(TEST_BYTE_PATH)
+
+
+def exe_to_bytes(exe_file, byte_file):
+    """
+    二进制文件 => 字节码序列文件
+    """
+    bytes = []
+    f_input = open(exe_file, 'rb')
+    f_input.seek(0, 0)
+    while True:
+        byte = f_input.read(1)
+        if len(byte) == 0:
+            break
+        else:
+            bytes.append('%02X' % ord(byte))
+    f_input.close()
+    f_output = open(byte_file, 'w+')
+    count = 0
+    lenth = len(bytes)
+    for byte in bytes:
+        count += 1
+        if count != lenth:
+            f_output.write(byte + ' ')
+        else:
+            f_output.write(byte)
+        if count % 16 == 0:
+            f_output.write('\n')
+    f_output.close() 
+
+
+def get_byte_files(exe_path, byte_path):
+    files = os.listdir(exe_path)
+    p = Pool(MAX_PROCESS_NUM)  # windows 下必须放在 “if __name__ == '__main__':” 后面，否则会报错
+    for file in files:
+        exe_file = os.path.join(exe_path, file)
+        byte_file = os.path.join(byte_path, file + '.txt')
+        p.apply_async(exe_to_bytes, args=(exe_file, byte_file,))
+    p.close()
+    p.join()
+
+
+if __name__ == '__main__':
+    get_byte_files(NORMAL_EXE_PATH, NORMAL_BYTE_PATH)
+    get_byte_files(MALWARE_EXE_PATH, MALWARE_BYTE_PATH)
