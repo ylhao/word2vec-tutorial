d57859caf489f249607e8c8ec0c6bde3b54eef15
yilonghao <765422195@qq.com>
2018-12-05 19:55:15 -0500

    2018/12/06

--- utils/exe_to_bytes.py
+++ utils/exe_to_bytes.py
@@ -12,7 +12,6 @@ NORMAL_BYTE_PATH = 'D:/malware_detection_dataset/normal_byte'
 MALWARE_BYTE_PATH = 'D:/malware_detection_dataset/malware_byte'
 # TEST_EXE_PATH = 'D:/malware_detection_dataset/test'
 # TEST_BYTE_PATH = 'D:/malware_detection_dataset/test_byte'
-MAX_PROCESS_NUM = 2  # 最大进程数
 
 
 # 如果文件夹不存在则创建对应的文件夹
@@ -24,47 +23,18 @@ if not os.path.exists(MALWARE_BYTE_PATH):
     # os.makedirs(TEST_BYTE_PATH)
 
 
-def exe_to_bytes(exe_file, byte_file):
+def exe_to_bytes(exe_file):
     """
     二进制文件 => 字节码序列文件
     """
-    bytes = []
-    f_input = open(exe_file, 'rb')
-    f_input.seek(0, 0)
-    while True:
-        byte = f_input.read(1)
-        if len(byte) == 0:
-            break
-        else:
-            bytes.append('%02X' % ord(byte))
-    f_input.close()
-    f_output = open(byte_file, 'w+')
-    count = 0
-    lenth = len(bytes)
-    for byte in bytes:
-        count += 1
-        if count != lenth:
-            f_output.write(byte + ' ')
-        else:
-            f_output.write(byte)
-        if count % 16 == 0:
-            f_output.write('\n')
-    f_output.close() 
-
-
-def get_byte_files(exe_path, byte_path):
-    files = os.listdir(exe_path)
-    p = Pool(MAX_PROCESS_NUM)  # windows 下必须放在 “if __name__ == '__main__':” 后面，否则会报错
-    for file in files:
-        if os.path.exists(os.path.join(byte_path, file + '.txt')):  # 跳过已有的文件
-            continue
-        exe_file = os.path.join(exe_path, file)
-        byte_file = os.path.join(byte_path, file + '.txt')
-        p.apply_async(exe_to_bytes, args=(exe_file, byte_file,))
-    p.close()
-    p.join()
-
-
+	with open(filename, 'rb') as f:
+        content = f.read()
+    hexst = binascii.hexlify(content)  # 将二进制文件转换为十六进制字符串
+    fh = np.array([int(hexst[i:i+2], 16) for i in range(0, len(hexst), 2)])  # 按字节分割
+    rn = len(fh)//width
+    fh = np.reshape(fh[:rn*width],(-1, width))  # 根据设定的宽度生成矩阵
+    return fh
+	
 if __name__ == '__main__':
     get_byte_files(NORMAL_EXE_PATH, NORMAL_BYTE_PATH)
     get_byte_files(MALWARE_EXE_PATH, MALWARE_BYTE_PATH)
--- utils/exe_to_img.py
+++ utils/exe_to_img.py
@@ -13,9 +13,6 @@ NORMAL_EXE_PATH = 'D:/malware_detection_dataset/normal'
 MALWARE_EXE_PATH = 'D:/malware_detection_dataset/malware'
 NORMAL_IMG_PATH = 'D:/malware_detection_dataset/normal_img'
 MALWARE_IMG_PATH = 'D:/malware_detection_dataset/malware_img'
-# TEST_EXE_PATH = 'D:/malware_detection_dataset/test'
-# TEST_IMG_PATH = 'D:/malware_detection_dataset/test_img'
-MAX_PROCESS_NUM = 2  # 最大进程数
 
 
 # 如果文件夹不存在则创建对应的文件夹
@@ -23,8 +20,6 @@ if not os.path.exists(NORMAL_IMG_PATH):
     os.makedirs(NORMAL_IMG_PATH)
 if not os.path.exists(MALWARE_IMG_PATH):
     os.makedirs(MALWARE_IMG_PATH)
-# if not os.path.exists(TEST_IMG_PATH):
-    # os.makedirs(TEST_IMG_PATH)
 
 
 def getMatrixfrom_bin(filename, width):
@@ -41,8 +36,8 @@ def getMatrixfrom_bin(filename, width):
 def get_img_files(exe_path, img_path):
     
     files = os.listdir(exe_path)
-    print(files)
     for file in files:
+        print(file)
         exe_file = os.path.join(exe_path, file)
         img_file = os.path.join(img_path, file + '.png')
         if os.path.exists(img_file):  # 判断是否已经生成过该 exe 文件对应的图片
@@ -72,19 +67,11 @@ def get_img_files(exe_path, img_path):
             else:
                 continue
             # 文件大小在合理范围内，执行以下语句
-            print('{} ==> {}'.format(file, file + '.png'))
             fh = getMatrixfrom_bin(exe_file, width)
             im = Image.fromarray(fh)  # 转换为图像
             im.save(img_file)
-            print('{} ==> {} done'.format(file, file + '.png'))
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    print('start time:', start_time)
-    # get_img_files(TEST_EXE_PATH, TEST_IMG_PATH)
     get_img_files(NORMAL_EXE_PATH, NORMAL_IMG_PATH)
-    #get_img_files(MALWARE_EXE_PATH, MALWARE_IMG_PATH)
-    end_time = time.time()
-    print('end time:', end_time)
-    print('time cost:', end_time - start_time)
+    get_img_files(MALWARE_EXE_PATH, MALWARE_IMG_PATH)
