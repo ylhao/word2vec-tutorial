ed1f14628e3c481802d6459793a44a33ec6cb721
yilonghao <765422195@qq.com>
2018-12-06 07:22:33 -0500

    2018/12/06

--- train.py
+++ train.py
@@ -181,8 +181,8 @@ def mergeFinetuneModel():
 
 if __name__ == '__main__':
 
-    #finetuneModel(inception_v3.preprocess_input, InceptionV3, 'inceptionv3', INCEPTIONV3_NO_TRAINABLE_LAYERS)
-    #finetuneModel(resnet50.preprocess_input, ResNet50, 'resnet50', RESNET50_NO_TRAINABLE_LAYERS)
+    finetuneModel(inception_v3.preprocess_input, InceptionV3, 'inceptionv3', INCEPTIONV3_NO_TRAINABLE_LAYERS)
+    finetuneModel(resnet50.preprocess_input, ResNet50, 'resnet50', RESNET50_NO_TRAINABLE_LAYERS)
     finetuneModel(xception.preprocess_input, Xception, 'xception', XCEPTION_NO_TRAINABLE_LAYERS)
     #extractFeatures()
     #mergeFinetuneModel()
